dac6b449a1ad4908be89eb7a807c68ba4dc9d66d
William Janelli <williamjanelli@yahoo.com>
2024-02-03 13:09:45 -0800

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-container">
+      {yesPressed ? (
+        <>
+          <img
+            src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+            alt="Bear kisses gif"
+            className="image-large"
+          />
+          <div className="text-large font-bold my-4">Ok yay!!!</div>
+        </>
+      ) : (
+        <>
+          <img
+            src="https://gifdb.com/images/high/cute-pusheen-valentine-s-day-pgyu3d9w4dmb1zyz.gif"
+            alt="Cute cat gif"
+            className="image-large"
+          />
+          <h1 className="text-large my-4">Will you be my Valentine?</h1>
+          <div>
+            <button
+              className="button-yes"
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              className="button-no"
+              onClick={handleNoClick}
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
